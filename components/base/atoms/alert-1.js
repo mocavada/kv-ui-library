5e3e9b476f0fbf394cc5ba7b02d60f9618ae1720
mocavada <cebuprint@gmail.com>
2023-03-31 15:19:12 -0400

    push

---
+++ components/base/atoms/alert-1.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Alert from '@mui/material/Alert';
+
+export default function Alert1() {
+  
+    return (
+      <Stack spacing={2} direction="row">
+        <Alert variant="text">Text</Alert>
+     
+      </Stack>
+    );
+  }
---
+++ components/base/atoms/button-1.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+export default function Button1() {
+  
+    return (
+      <Stack spacing={2} direction="row">
+        
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+    );
+  }
